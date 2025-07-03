012cccd0c56a1b57090231bf581bb75930cc103f
Dariusz Okoński <darek200180@gmail.com>
2025-07-03 08:35:55 +0200

    prepare weatherData object

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -11,7 +11,28 @@ const WeatherBox = (props) => {
       return;
     }
 
-    console.log('WeatherBox city: ', city);
+    setIsLoading(true);
+
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a9302906cfdb7f28449954dd9590d745&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.cod === '404') {
+          throw new Error(data.message);
+        }
+
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        };
+
+        console.log('weatherData: ', weatherData);
+      })
+      .catch((err) => console.log(err.message))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
