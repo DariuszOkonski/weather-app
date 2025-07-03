b031b6260f885d0a0174e31a6aa863396744d549
Dariusz Okoński <darek200180@gmail.com>
2025-07-03 08:14:31 +0200

    provide city to WeatherBox component

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,14 +2,18 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 
-const WeatherBox = props => {
+const WeatherBox = (props) => {
+  const handleSubmit = (city) => {
+    console.log('WeatherBox city: ', city);
+  };
+
   return (
     <section>
-      <PickCity />
+      <PickCity onSubmit={handleSubmit} />
       <WeatherSummary />
       <Loader />
     </section>
-  )
+  );
 };
 
 export default WeatherBox;
